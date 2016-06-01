3f6325a7fdf38c411438bfa1de0f53fd128852db
RossignolVincent <rossignol.vince@gmail.com>
2016-06-01 18:29:44 +0200

    Test changes

--- test/index.specs.js
+++ test/index.specs.js
@@ -5,7 +5,7 @@ describe('contacts', function(){
     describe('GET api/contacts', function(){
         it('should return the list of contacts', function(){
             return request(api)
-            .get('api/contacts')
+            .get('/api/contacts')
             .send()
             .expect(200)
         })
@@ -14,14 +14,14 @@ describe('contacts', function(){
     describe('POST api/contacts/:name', function(){
         it('should create a new contact', function(){
             return request(api)
-            .post('api/contacts/foo')
+            .post('/api/contacts/foo')
             .send()
             .expect(200)
         })
         
         it('should not autorize to create a new contact with an alread registered name', function(){
             return request(api)
-            .post('api/contacts/exist')
+            .post('/api/contacts/exist')
             .send()
             .expect(403)
         })
@@ -31,7 +31,7 @@ describe('contacts', function(){
     describe('PUT api/contacts/:name/:new', function(){
         it('should update contacts with the same name', function(){
             return request(api)
-            .put("api/contacts/foo/bar")
+            .put("/api/contacts/foo/bar")
             .send()
             .expect(200)
         })
@@ -40,7 +40,7 @@ describe('contacts', function(){
     describe('DELETE api/contacts/:name', function(){
         it('should remove contacts with the same name', function(){
             return request(api)
-            .delete("api/contacts/foo")
+            .delete("/api/contacts/foo")
             .send()
             .expect(200)
         })
