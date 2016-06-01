7fbadb0c74d2d173ad57030d538a86f7cd8be698
Vincent Rossignol <rossignol.vince@gmail.com>
2016-06-01 16:28:24 +0200

    tests for contacts

--- test/index.specs.js
+++ test/index.specs.js
@@ -1,11 +1,40 @@
 var request = require('supertest')
 var api = require("../index")
 
-describe('api', function(){
-    it('should return Hello World on GET /', function(){
-        return request(api)
-        .get('/')
-        .send()
-        .expect('Hello foobar')
+describe('contacts', function(){
+    describe('GET /contacts', function(){
+        it('should return the list of contacts', function(){
+            return request(api)
+            .get('/contacts')
+            .send()
+            .expect(200)
+        })
+    })
+    
+    describe('POST /contacts/:name', function(){
+        it('should create a new contact', function(){
+            return request(api)
+            .post('/contacts/foo')
+            .send()
+            .expect(200)
+        })
+    })
+    
+    describe('PUT /contacts/:name/:new', function(){
+        it('should update contacts with the same name', function(){
+            return request(api)
+            .put("/contacts/foo/bar")
+            .send()
+            .expect(200)
+        })
+    })
+    
+    describe('DELETE /contacts/:name', function(){
+        it('should remove contacts with the same name', function(){
+            return request(api)
+            .delete("/contacts/foo")
+            .send()
+            .expect(200)
+        })
     })
 })
