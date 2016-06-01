a917dfb5a175b414ec0a33f36ec8c9f2a24c5b31
RossignolVincent <rossignol.vince@gmail.com>
2016-06-01 18:32:57 +0200

    Test changes

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ var DEFAULT_PORT = 3000;
 
 api.use(express.static('public'))
 
-api.get('api/', function(req, res, next){
+api.get('/api/', function(req, res, next){
     res.send("Hello Vincent !")
 })
 
-api.get('api/contacts', function(req, res, next){
+api.get('/api/contacts', function(req, res, next){
     res.send([])
 })
 
-api.get('api/contacts/:name', function(req, res, next){
+api.get('/api/contacts/:name', function(req, res, next){
     res.send()
 })
 
-api.post('api/contacts/:name', function(req, res, next){
+api.post('/api/contacts/:name', function(req, res, next){
     if(req.params.name === 'exist')
         return res.status(403).send()
     res.send()
 })
 
-api.put('api/contacts/:name/:new', function(req, res, next){
+api.put('/api/contacts/:name/:new', function(req, res, next){
     res.send()
 })
 
-api.delete('api/contacts/:name', function(req, res, next){
+api.delete('/api/contacts/:name', function(req, res, next){
     res.send()
 })
 
