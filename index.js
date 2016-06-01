ccf5890d24f77d161c70704f1470acfc85203705
Vincent Rossignol <rossignol.vince@gmail.com>
2016-06-01 17:08:38 +0200

    updated port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var api = express()
+var DEFAULT_PORT = 3000;
 
 api.get('/', function(req, res, next){
     res.send('Hello world')
 })
 
-console.log('server started on port 3000')
-api.listen(3000)
+var port = process.env.PORT || DEFAULT_PORT
+console.log('server started on port '+port)
+api.listen(port)
 
 module.exports = api
